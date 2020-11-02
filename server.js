a52f1c8e06da2c71fe27d6feb93d77c40426b5c3
tranvikhan <tranvikhan@gmail.com>
2020-11-02 22:30:58 +0700

    oke

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/demo", (req, res) => {
 // routes
 routes(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
